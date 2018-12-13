a8f3051bccd3566e731ad957286609acc1a997dd
nvdinh185 <nvdinh0905671805@gmail.com>
2018-12-13 11:27:44 +0700

    push to git

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "push to git"
-git remote add origin https://github.com/nvdinh185/Bai6HocVienReactive.git
+#git remote add origin https://github.com/nvdinh185/Angular-HocVienReactive.git
 git push -u origin master
